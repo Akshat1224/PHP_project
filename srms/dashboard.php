3fc313ccdea35cb3bcd61651bad32a484317d7f8
Akshat Kushwaha <akshatkushwaha7oct2003@gmail.com>
2024-11-28 10:10:43 +0530

    commit

--- srms/dashboard.php
+++ srms/dashboard.php
@@ -11,125 +11,189 @@ if(strlen($_SESSION['alogin'])=="")
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Student Result Management System | Dashboard</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background: linear-gradient(120deg, #f6d365, #fda085);
-                margin: 0;
-                color: #333;
-            }
-            .main-wrapper {
-                max-width: 1200px;
-                margin: auto;
-                padding: 20px;
-            }
-            .title {
-                color: #333;
-                font-size: 28px;
-                font-weight: bold;
-            }
-            .dashboard-stat {
-                text-decoration: none;
-                display: block;
-                padding: 20px;
-                border-radius: 10px;
-                background: white;
-                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-                margin-bottom: 20px;
-                position: relative;
-                transition: transform 0.3s ease;
-            }
-            .dashboard-stat:hover {
-                transform: translateY(-5px);
-                box-shadow: 0 6px 10px rgba(0, 0, 0, 0.15);
-            }
-            .dashboard-stat .number {
-                font-size: 36px;
-                font-weight: bold;
-                color: #555;
-            }
-            .dashboard-stat .name {
-                font-size: 18px;
-                color: #777;
-            }
-            .dashboard-stat .bg-icon {
-                font-size: 50px;
-                color: #f6d365;
-                position: absolute;
-                top: 10px;
-                right: 10px;
-            }
-            .bg-primary {
-                border-left: 5px solid #6a5acd;
-            }
-            .bg-danger {
-                border-left: 5px solid #e74c3c;
-            }
-            .bg-warning {
-                border-left: 5px solid #f39c12;
-            }
-            .bg-success {
-                border-left: 5px solid #2ecc71;
-            }
-        </style>
+        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
+        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
+        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
+        <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen" >
+        <link rel="stylesheet" href="css/toastr/toastr.min.css" media="screen" >
+        <link rel="stylesheet" href="css/icheck/skins/line/blue.css" >
+        <link rel="stylesheet" href="css/icheck/skins/line/red.css" >
+        <link rel="stylesheet" href="css/icheck/skins/line/green.css" >
+        <link rel="stylesheet" href="css/main.css" media="screen" >
+        <script src="js/modernizr/modernizr.min.js"></script>
     </head>
-    <body>
+    <body class="top-navbar-fixed">
         <div class="main-wrapper">
-            <div class="content-container">
-                <h2 class="title">Dashboard</h2>
-                <div class="row">
-                    <div class="dashboard-stat bg-primary">
-                        <?php 
-                        $sql1 ="SELECT StudentId from tblstudents ";
-                        $query1 = $dbh -> prepare($sql1);
-                        $query1->execute();
-                        $totalstudents=$query1->rowCount();
-                        ?>
-                        <span class="number counter"><?php echo htmlentities($totalstudents);?></span>
-                        <span class="name">Registered Users</span>
-                        <span class="bg-icon"><i class="fa fa-users"></i></span>
-                    </div>
+              <?php include('includes/topbar.php');?>
+            <div class="content-wrapper">
+                <div class="content-container">
 
-                    <div class="dashboard-stat bg-danger">
-                        <?php 
-                        $sql ="SELECT id from tblsubjects ";
-                        $query = $dbh -> prepare($sql);
-                        $query->execute();
-                        $totalsubjects=$query->rowCount();
-                        ?>
-                        <span class="number counter"><?php echo htmlentities($totalsubjects);?></span>
-                        <span class="name">Subjects Listed</span>
-                        <span class="bg-icon"><i class="fa fa-ticket"></i></span>
-                    </div>
+                    <?php include('includes/leftbar.php');?>  
 
-                    <div class="dashboard-stat bg-warning">
-                        <?php 
-                        $sql2 ="SELECT id from tblclasses ";
-                        $query2 = $dbh -> prepare($sql2);
-                        $query2->execute();
-                        $totalclasses=$query2->rowCount();
-                        ?>
-                        <span class="number counter"><?php echo htmlentities($totalclasses);?></span>
-                        <span class="name">Total Classes</span>
-                        <span class="bg-icon"><i class="fa fa-bank"></i></span>
-                    </div>
+                    <div class="main-page">
+                        <div class="container-fluid">
+                            <div class="row page-title-div">
+                                <div class="col-sm-6">
+                                    <h2 class="title">Dashboard</h2>
+                                  
+                                </div>
+                                <!-- /.col-sm-6 -->
+                            </div>
+                            <!-- /.row -->
+                      
+                        </div>
+                        <!-- /.container-fluid -->
 
-                    <div class="dashboard-stat bg-success">
-                        <?php 
-                        $sql3="SELECT distinct StudentId from tblresult ";
-                        $query3 = $dbh -> prepare($sql3);
-                        $query3->execute();
-                        $totalresults=$query3->rowCount();
-                        ?>
-                        <span class="number counter"><?php echo htmlentities($totalresults);?></span>
-                        <span class="name">Results Declared</span>
-                        <span class="bg-icon"><i class="fa fa-file-text"></i></span>
+                        <section class="section">
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                        <a class="dashboard-stat bg-primary" href="manage-students.php">
+<?php 
+$sql1 ="SELECT StudentId from tblstudents ";
+$query1 = $dbh -> prepare($sql1);
+$query1->execute();
+$results1=$query1->fetchAll(PDO::FETCH_OBJ);
+$totalstudents=$query1->rowCount();
+?>
+
+                                            <span class="number counter"><?php echo htmlentities($totalstudents);?></span>
+                                            <span class="name">Regd Users</span>
+                                            <span class="bg-icon"><i class="fa fa-users"></i></span>
+                                        </a>
+                                        <!-- /.dashboard-stat -->
+                                    </div>
+                                    <!-- /.col-lg-3 col-md-3 col-sm-6 col-xs-12 -->
+
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" >
+                                        <a class="dashboard-stat bg-danger" href="manage-subjects.php">
+<?php 
+$sql ="SELECT id from  tblsubjects ";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$totalsubjects=$query->rowCount();
+?>
+                                            <span class="number counter"><?php echo htmlentities($totalsubjects);?></span>
+                                            <span class="name">Subjects Listed</span>
+                                            <span class="bg-icon"><i class="fa fa-ticket"></i></span>
+                                        </a>
+                                        <!-- /.dashboard-stat -->
+                                    </div>
+                                    <!-- /.col-lg-3 col-md-3 col-sm-6 col-xs-12 -->
+
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="margin-top:1%;">
+                                        <a class="dashboard-stat bg-warning" href="manage-classes.php">
+                                        <?php 
+$sql2 ="SELECT id from  tblclasses ";
+$query2 = $dbh -> prepare($sql2);
+$query2->execute();
+$results2=$query2->fetchAll(PDO::FETCH_OBJ);
+$totalclasses=$query2->rowCount();
+?>
+                                            <span class="number counter"><?php echo htmlentities($totalclasses);?></span>
+                                            <span class="name">Total classes listed</span>
+                                            <span class="bg-icon"><i class="fa fa-bank"></i></span>
+                                        </a>
+                                        <!-- /.dashboard-stat -->
+                                    </div>
+                                    <!-- /.col-lg-3 col-md-3 col-sm-6 col-xs-12 -->
+
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12"  style="margin-top:1%">
+                                        <a class="dashboard-stat bg-success" href="manage-results.php">
+                                        <?php 
+$sql3="SELECT  distinct StudentId from  tblresult ";
+$query3 = $dbh -> prepare($sql3);
+$query3->execute();
+$results3=$query3->fetchAll(PDO::FETCH_OBJ);
+$totalresults=$query3->rowCount();
+?>
+
+                                            <span class="number counter"><?php echo htmlentities($totalresults);?></span>
+                                            <span class="name">Results Declared</span>
+                                            <span class="bg-icon"><i class="fa fa-file-text"></i></span>
+                                        </a>
+                                        <!-- /.dashboard-stat -->
+                                    </div>
+                                    <!-- /.col-lg-3 col-md-3 col-sm-6 col-xs-12 -->
+
+                                </div>
+                                <!-- /.row -->
+                            </div>
+                            <!-- /.container-fluid -->
+                        </section>
+                        <!-- /.section -->
+
                     </div>
+                    <!-- /.main-page -->
+
+                    
                 </div>
+                <!-- /.content-container -->
             </div>
+            <!-- /.content-wrapper -->
+
         </div>
+        <!-- /.main-wrapper -->
+
+        <!-- ========== COMMON JS FILES ========== -->
+        <script src="js/jquery/jquery-2.2.4.min.js"></script>
+        <script src="js/jquery-ui/jquery-ui.min.js"></script>
+        <script src="js/bootstrap/bootstrap.min.js"></script>
+        <script src="js/pace/pace.min.js"></script>
+        <script src="js/lobipanel/lobipanel.min.js"></script>
+        <script src="js/iscroll/iscroll.js"></script>
+
+        <!-- ========== PAGE JS FILES ========== -->
+        <script src="js/prism/prism.js"></script>
+        <script src="js/waypoint/waypoints.min.js"></script>
+        <script src="js/counterUp/jquery.counterup.min.js"></script>
+        <script src="js/amcharts/amcharts.js"></script>
+        <script src="js/amcharts/serial.js"></script>
+        <script src="js/amcharts/plugins/export/export.min.js"></script>
+        <link rel="stylesheet" href="js/amcharts/plugins/export/export.css" type="text/css" media="all" />
+        <script src="js/amcharts/themes/light.js"></script>
+        <script src="js/toastr/toastr.min.js"></script>
+        <script src="js/icheck/icheck.min.js"></script>
+
+        <!-- ========== THEME JS ========== -->
+        <script src="js/main.js"></script>
+        <script src="js/production-chart.js"></script>
+        <script src="js/traffic-chart.js"></script>
+        <script src="js/task-list.js"></script>
+        <script>
+            $(function(){
+
+                // Counter for dashboard stats
+                $('.counter').counterUp({
+                    delay: 10,
+                    time: 1000
+                });
+
+                // Welcome notification
+                toastr.options = {
+                  "closeButton": true,
+                  "debug": false,
+                  "newestOnTop": false,
+                  "progressBar": false,
+                  "positionClass": "toast-top-right",
+                  "preventDuplicates": false,
+                  "onclick": null,
+                  "showDuration": "300",
+                  "hideDuration": "1000",
+                  "timeOut": "5000",
+                  "extendedTimeOut": "1000",
+                  "showEasing": "swing",
+                  "hideEasing": "linear",
+                  "showMethod": "fadeIn",
+                  "hideMethod": "fadeOut"
+                }
+                toastr["success"]( "Welcome to student Result Management System!");
+
+            });
+        </script>
     </body>
 </html>
 <?php } ?>
